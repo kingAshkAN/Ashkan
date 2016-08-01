9e095adac6f3a8b43788a219f95c8cad15a742d7
kingAshkAN <Ashkankingtelegram@gmail.com>
2016-08-01 10:03:03 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "id",
     "admin"
     },
-    sudo_users = {85831686},--Sudo users
+    sudo_users = {188376409},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[spammer-bot
